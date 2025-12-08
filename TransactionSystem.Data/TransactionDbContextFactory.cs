b310d4ab335fecdaddabd5247116e746f271ecab
tozobg <tozobg@yahoo.com>
2025-12-08 12:23:16 +0200

    add comment to TransactionDbContextFactory

--- TransactionSystem.Data/TransactionDbContextFactory.cs
+++ TransactionSystem.Data/TransactionDbContextFactory.cs
@@ -1,4 +1,10 @@
-﻿//using Microsoft.EntityFrameworkCore;
+﻿// This class is only needed if you want to create the database using EF Core migrations.
+// To use it:
+// 1. Uncomment this file.
+// 2. Make sure to comment out `context.Database.EnsureCreated()` in Startup.cs
+
+
+//using Microsoft.EntityFrameworkCore;
 //using Microsoft.EntityFrameworkCore.Design;
 
 //namespace TransactionSystem.Data
