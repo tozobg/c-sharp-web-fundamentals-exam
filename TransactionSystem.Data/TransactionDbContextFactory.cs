8389141e4bddc41b057d376d6be2e61ac0ff0493
tozobg <tozobg@yahoo.com>
2025-12-08 12:17:28 +0200

    ensure database created

---
+++ TransactionSystem.Data/TransactionDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Design;
+
+//namespace TransactionSystem.Data
+//{
+//    public class TransactionDbContextFactory : IDesignTimeDbContextFactory<TransactionDbContext>
+//    {
+//        public TransactionDbContext CreateDbContext(string[] args)
+//        {
+//            var optionsBuilder = new DbContextOptionsBuilder<TransactionDbContext>();
+//            optionsBuilder.UseSqlite("Data Source=Transactions.db");
+
+//            return new TransactionDbContext(optionsBuilder.Options);
+//        }
+//    }
+//}
--- TransactionSystem/StartUp.cs
+++ TransactionSystem/StartUp.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using TransactionSystem.Core;
 using TransactionSystem.Core.Interfaces;
+using TransactionSystem.Data;
 using TransactionSystem.IO;
 
 namespace TransactionSystem
@@ -9,6 +11,14 @@ namespace TransactionSystem
     {
         static void Main(string[] args)
         {
+            var optionsBuilder = new DbContextOptionsBuilder<TransactionDbContext>();
+            optionsBuilder.UseSqlite("Data Source=Transactions.db");
+
+            using var context = new TransactionDbContext(optionsBuilder.Options);
+
+            // Ensure the database is created
+            context.Database.EnsureCreated();
+
             // Create engine with reader and writer
             IEngine engine = new Engine(new ConsoleReader(), new ConsoleWriter());
 
