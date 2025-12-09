b0f6a764336c981e2351ecf57b075be531d2d9f6
tozobg <tozobg@yahoo.com>
2025-12-09 11:59:53 +0200

    Add unit tests

---
+++ TransactionSystem.Tests/Services/AccountServiceTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransactionSystem.Core.DTOs.Input;
+using TransactionSystem.Core.Services;
+using TransactionSystem.Data.InMemory.UnitOfWork;
+
+namespace TransactionSystem.Tests.Services
+{
+    [TestFixture]
+    public class AccountServiceTests
+    {
+        private InMemoryUnitOfWork _uow = null!;
+        private AccountService _service = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uow = new InMemoryUnitOfWork();
+            _service = new AccountService(_uow);
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            _uow.Dispose();
+        }
+
+        [Test]
+        public async Task CreateAccount_Should_Add_Account()
+        {
+            var input = new CreateAccountDto
+            {
+                AccountNumber = 1,
+                FullName = "Anatoli Ivanov",
+                Balance = 200
+            };
+
+            var result = await _service.CreateAccountAsync(input);
+
+            Assert.That(result.AccountNumber, Is.EqualTo(input.AccountNumber));
+            Assert.That(result.FullName, Is.EqualTo(input.FullName));
+            Assert.That(result.Balance, Is.EqualTo(input.Balance));
+
+            var allAccounts = await _service.GetAllAccountsAsync();
+            Assert.That(allAccounts.Count(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public async Task CreateAccount_Should_Throw_When_AccountNumberExists()
+        {
+            var input = new CreateAccountDto
+            {
+                AccountNumber = 1,
+                FullName = "Petyr Petrov",
+                Balance = 200
+            };
+
+            await _service.CreateAccountAsync(input);
+
+            var duplicate = new CreateAccountDto
+            {
+                AccountNumber = 1,
+                FullName = "Petyr Ivanov",
+                Balance = 300
+            };
+
+            Assert.ThrowsAsync<Exception>(async () =>
+            {
+                await _service.CreateAccountAsync(duplicate);
+            });
+        }
+
+        [Test]
+        public async Task GetAllAccounts_ShouldReturnMultipleAccounts()
+        {
+            await _service.CreateAccountAsync(new CreateAccountDto
+            {
+                AccountNumber = 1,
+                FullName = "Petyr Petrov",
+                Balance = 100
+            });
+            await _service.CreateAccountAsync(new CreateAccountDto
+            {
+                AccountNumber = 2,
+                FullName = "Petyr Ivanov",
+                Balance = 200
+            });
+
+            var all = await _service.GetAllAccountsAsync();
+            Assert.That(all.Count(), Is.EqualTo(2));
+            Assert.IsTrue(all.Any(a => a.FullName == "Petyr Petrov"));
+            Assert.IsTrue(all.Any(a => a.FullName == "Petyr Ivanov"));
+        }
+    }
+}
---
+++ TransactionSystem.Tests/Services/TransactionServiceTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransactionSystem.Core.DTOs.Input;
+using TransactionSystem.Core.Services;
+using TransactionSystem.Data.InMemory.UnitOfWork;
+
+namespace TransactionSystem.Tests.Services
+{
+    public class TransactionServiceTests
+    {
+        private InMemoryUnitOfWork _uow = null!;
+        private AccountService _accountService = null!;
+        private TransactionService _transactionService = null!;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _uow = new InMemoryUnitOfWork();
+            _accountService = new AccountService(_uow);
+            _transactionService = new TransactionService(_uow);
+
+            // Add two accounts for tests
+            await _accountService.CreateAccountAsync(new CreateAccountDto
+            {
+                AccountNumber = 1,
+                FullName = "Petyr Petrov",
+                Balance = 1000
+            });
+
+            await _accountService.CreateAccountAsync(new CreateAccountDto
+            {
+                AccountNumber = 2,
+                FullName = "Ivan Ivanov",
+                Balance = 500
+            });
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            _uow.Dispose();
+        }
+
+        [Test]
+        public async Task Deposit_Should_Increase_Balance()
+        {
+            await _transactionService.DepositAsync(new TransactionInputDto
+            {
+                AccountNumber = 1,
+                Amount = 200
+            });
+
+            var account = await _uow.Accounts.GetByAccountNumberAsync(1);
+            Assert.That(account!.Balance, Is.EqualTo(1200));
+        }
+
+        [Test]
+        public async Task Withdraw_Should_Decrease_Balance()
+        {
+            await _transactionService.WithdrawAsync(new TransactionInputDto
+            {
+                AccountNumber = 1,
+                Amount = 300
+            });
+
+            var account = await _uow.Accounts.GetByAccountNumberAsync(1);
+            Assert.That(account!.Balance, Is.EqualTo(700));
+        }
+
+        [Test]
+        public void Withdraw_Should_Throw_When_InsufficientFunds()
+        {
+            Assert.ThrowsAsync<Exception>(async () =>
+            {
+                await _transactionService.WithdrawAsync(new TransactionInputDto
+                {
+                    AccountNumber = 2,
+                    Amount = 1000
+                });
+            });
+        }
+
+        [Test]
+        public async Task Transfer_Should_Move_Money_Between_Accounts()
+        {
+            await _transactionService.TransferAsync(new TransferInputDto
+            {
+                FromAccountNumber = 1,
+                ToAccountNumber = 2,
+                Amount = 400
+            });
+
+            var from = await _uow.Accounts.GetByAccountNumberAsync(1);
+            var to = await _uow.Accounts.GetByAccountNumberAsync(2);
+
+            Assert.That(from!.Balance, Is.EqualTo(600));
+            Assert.That(to!.Balance, Is.EqualTo(900));
+        }
+
+        [Test]
+        public void Transfer_Should_Throw_When_InsufficientFunds()
+        {
+            Assert.ThrowsAsync<Exception>(async () =>
+            {
+                await _transactionService.TransferAsync(new TransferInputDto
+                {
+                    FromAccountNumber = 2,
+                    ToAccountNumber = 1,
+                    Amount = 1000
+                });
+            });
+        }
+    }
+}
